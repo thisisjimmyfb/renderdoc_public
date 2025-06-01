f64d273bc6e6d68ba0d24b496c20fcda29f8f379
thisisjimmyfb <58957694+thisisjimmyfb@users.noreply.github.com>
2025-06-01 16:59:42 -0700

    VK_Dedicated_Allocation

--- util/test/tests/Vulkan/VK_Dedicated_Allocation.py
+++ util/test/tests/Vulkan/VK_Dedicated_Allocation.py
@@ -44,12 +44,24 @@ class VK_Dedicated_Allocation(rdtest.TestCase):
         rdtest.log.success('Mesh data is as expected')
 
         pipe: rd.PipeState = self.controller.GetPipelineState()
+        res = pipe.GetOutputTargets()[0].resource
 
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 155, 195, [1.0, 0.0, 0.09, 1.0], eps=1.0/255.0)
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 190, 195, [0.0, 1.0, 0.09, 1.0], eps=1.0/255.0)
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 255, 195, [1.0, 0.0, 0.09, 1.0], eps=1.0/255.0)
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 230, 150, [0.723, 1.0, 1.0, 1.0], eps=1.0/255.0)
+        coords = self.screen_crop_coords(res)
+        x_coord = lambda x: int((float(x)/400.0)*coords[2]+coords[0])
+        y_coord = lambda y: int((float(y)/300.0)*coords[3]+coords[1])
 
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 190, 80, [0.2, 0.2, 0.2, 1.0], eps=1.0/255.0)
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 200, 80, [0.723, 1.0, 1.0, 1.0], eps=1.0/255.0)
-        self.check_pixel_value(pipe.GetOutputTargets()[0].resource, 210, 80, [0.2, 0.2, 0.2, 1.0], eps=1.0/255.0)
+        self.check_pixel_value(res, x_coord(155), y_coord(195),
+                               [1.0, 0.0, 0.09, 1.0], eps=1.0/255.0)
+        self.check_pixel_value(res, x_coord(190), y_coord(195),
+                               [0.0, 1.0, 0.09, 1.0], eps=1.0/255.0)
+        self.check_pixel_value(res, x_coord(255), y_coord(195),
+                               [1.0, 0.0, 0.09, 1.0], eps=1.0/255.0)
+        self.check_pixel_value(res, x_coord(230), y_coord(150),
+                               [0.723, 1.0, 1.0, 1.0], eps=1.0/255.0)
+
+        self.check_pixel_value(res, x_coord(190), y_coord(80),
+                               [0.2, 0.2, 0.2, 1.0], eps=1.0/255.0)
+        self.check_pixel_value(res, x_coord(200), y_coord(80),
+                               [0.723, 1.0, 1.0, 1.0], eps=1.0/255.0)
+        self.check_pixel_value(res, x_coord(210), y_coord(80),
+                               [0.2, 0.2, 0.2, 1.0], eps=1.0/255.0)
