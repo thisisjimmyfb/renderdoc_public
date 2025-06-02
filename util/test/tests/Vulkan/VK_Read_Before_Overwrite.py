071f843a2b533ccaad375b4725b18b63e8a6dc56
thisisjimmyfb <58957694+thisisjimmyfb@users.noreply.github.com>
2025-06-01 17:08:38 -0700

    VK_Read_Before_Overwrite

--- util/test/tests/Vulkan/VK_Read_Before_Overwrite.py
+++ util/test/tests/Vulkan/VK_Read_Before_Overwrite.py
@@ -12,7 +12,8 @@ class VK_Read_Before_Overwrite(rdtest.TestCase):
         self.controller.SetFrameEvent(self.find_action("checkpoint").eventId, True)
         self.controller.SetFrameEvent(self.find_action("checkpoint").eventId, True)
 
-        self.check_triangle(None, [0.2, 0.2, 0.2, 1.0], [1.0, 1.0, 1.0, 1.0], (0.0, 0.0, 200.0, 300.0))
-        self.check_triangle(None, [0.2, 0.2, 0.2, 1.0], [1.0, 1.0, 1.0, 1.0], (200.0, 0.0, 200.0, 300.0))
+        vp = self.screen_crop_coords()
+        self.check_triangle(None, [0.2, 0.2, 0.2, 1.0], [1.0, 1.0, 1.0, 1.0], (0.0, 0.0, vp[2]*0.5, vp[3]))
+        self.check_triangle(None, [0.2, 0.2, 0.2, 1.0], [1.0, 1.0, 1.0, 1.0], (vp[2]*0.5, 0.0, vp[2]*0.5, vp[3]))
 
         rdtest.log.success('Triangle is rendered correctly')
