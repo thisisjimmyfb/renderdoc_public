948f636637a87b2edcb1ca1848ae961d06810d2e
thisisjimmyfb <58957694+thisisjimmyfb@users.noreply.github.com>
2025-06-01 17:11:16 -0700

    VK_Synchronization_2

--- util/test/tests/Vulkan/VK_Synchronization_2.py
+++ util/test/tests/Vulkan/VK_Synchronization_2.py
@@ -4,6 +4,7 @@ import renderdoc as rd
 
 class VK_Synchronization_2(rdtest.TestCase):
     demos_test_name = 'VK_Synchronization_2'
+    demos_frame_cap = 10
 
     def get_capture_options(self):
         opts = rd.CaptureOptions()
